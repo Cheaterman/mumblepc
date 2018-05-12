9fe067b80997a2e24ad3f7096273c6b8130d9c97
The Cheaterman <the.cheaterman@gmail.com>
2018-05-13 00:22:27 +0200

    Remove subprocess call to ffmpeg in favor of PyAv

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
+from av.audio.resampler import AudioResampler
 from pymumble_py3 import Mumble
+import av
+import io
 import subprocess
 import sys
 
@@ -13,18 +16,19 @@ client = Mumble(mumble_address, 'MumblePC', int(mumble_port))
 client.start()
 client.is_ready()
 
-process = subprocess.Popen(
-    ('ffmpeg -i %s -ac 1 -f s16le -ar 48000 -' % radio_address).split(),
-    stdout=subprocess.PIPE,
-    stderr=subprocess.PIPE,
-    bufsize=CHUNK_SIZE,
+container = av.open(radio_address)
+stream = next(
+    stream for stream in container.streams if stream.type == 'audio'
 )
+resampler = AudioResampler('s16p', 1, 48000)
 
-while process:
-    rawchunk = process.stdout.read(CHUNK_SIZE)
+quit = False
 
-    if rawchunk:
-        client.sound_output.add_sound(rawchunk)
-    else:
-        process.kill()
-        process = None
+while not quit:
+    for packet in container.demux(stream):
+        for frame in packet.decode():
+            frame.pts = None
+            frame = resampler.resample(frame)
+            rawchunk = frame.to_nd_array().tobytes()
+            client.sound_output.add_sound(rawchunk)
+    quit = True
